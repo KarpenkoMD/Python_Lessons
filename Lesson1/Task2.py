81d42cbe3140392958343c0ebe573de13c6c0efd
mikhailka <karpenko.md@gmail.com>
2022-07-10 20:21:25 +0300

    Task2 recursion try failed

--- Lesson1/Task2.py
+++ Lesson1/Task2.py
@@ -12,3 +12,15 @@ for x in range(2):
             claim_2 = not bool(x) and not bool(y) and not bool(z)
             print(f'При значении предикат X, Y, Z = [{bool(x)}, {bool(y)}, {bool(z)}]', end=', ')
             print(f'утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z = {claim_1 == claim_2}')
+
+""" def try_claim(preds = [False, False, False], length=0):
+    if length == len(preds):
+        claim_1 = not (preds[0] or preds[1] or preds[2])
+        claim_2 = not preds[0] and not preds[1] and not preds[2]
+        print(f'При значении предикат X, Y, Z = [{preds[0]}, {preds[1]}, {preds[2]}]', end=', ')
+        print(f'утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z = {claim_1 == claim_2}') 
+    for k in range(2):
+        preds[length] = bool(k)
+        try_claim (preds, length+1)
+
+try_claim() """
