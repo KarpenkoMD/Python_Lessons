c2cb0098d64e32430a7db2e5d7310b30a17a575c
mikhailka <karpenko.md@gmail.com>
2022-07-14 16:38:35 +0300

    Task4 finished

--- Lesson2/Task4.py
+++ Lesson2/Task4.py
@@ -2,3 +2,18 @@
 Реализуйте выдачу случайного числа (или алгоритм перемешивания списка)
 не использовать random.randint и вообще библиотеку random
 Можете использовать xor, биты, библиотеку time (миллисекунды или наносекунды) - для задания случайности """
+
+import time 
+
+def my_random(min=0, max=10):
+    result = min-1
+    digit_num = len(str(max))
+    while result < min or result > max:
+        string_time = str(time.time_ns()//100)
+        result = int(string_time[len(string_time)-digit_num:len(string_time)])
+    return result
+
+min_value = 1 
+max_value = 100
+
+print(my_random(min_value,max_value))
