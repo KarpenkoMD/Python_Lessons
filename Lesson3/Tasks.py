a00ddc10ecdec94fe76b6999acf0b7abb7a6160d
mikhailka <karpenko.md@gmail.com>
2022-07-16 20:15:21 +0300

    case1 finished

--- Lesson3/Tasks.py
+++ Lesson3/Tasks.py
@@ -14,20 +14,33 @@ while True:
     input_key = input()
     match input_key:
         case '1':
-            n = my_method.input_number('Введите число')
-            result_string = 'Задача 1'
+            list_size = my_method.input_number('Введите количество элементов списка: ','natural')
+            list_numbers = my_method.list_random_fill(list_size,0,20)
+            sum_odd_elements = my_method.get_sum_odd_elements(list_numbers)
+            result_string = 'Сумма нечетных элементов списка ' + str(list_numbers)\
+                 + ' равна ' + str(sum_odd_elements)
             break
         case '2':
-            result_string = 'Задача 2'
+            list_size = my_method.input_number('Введите количество элементов списка: ','natural')
+            list_numbers = my_method.list_random_fill(list_size,0,20)
+            result_string = str(list_numbers) + str(list_size)
             break
         case '3':
-            result_string = 'Задача 3'
+            list_size = my_method.input_number('Введите количество элементов списка: ','natural')
+            list_numbers = my_method.list_random_fill(list_size,0,20.0)
+            result_string = str(list_numbers) + str(list_size)
             break
         case '4':
-            result_string = 'Задача 4'
+            decimal_number = my_method.input_number(\
+                'Введите десятичное число для перевода в двоичное: ','int')
+            binary_number = my_method.dec_to_bin(decimal_number)
+            result_string = 'Десятичное число ' + str(decimal_number) +\
+                 ' = ' + binary_number + ' в двоичном виде'
             break
         case '5':
-            result_string = 'Задача 4'
+            number = my_method.input_number(\
+                'Введите число для ряда Фибоначчи: ','natural')
+            result_string = 'Задача 5'
             break
         case 'q':
             input_key = None
--- Lesson3/my_method.py
+++ Lesson3/my_method.py
@@ -1,11 +1,56 @@
-def input_number (input_message): 
-    while True: 
+import random
+
+
+def input_number(input_message, number_type = 'int'):
+    while True:
         input_str = input(input_message)
-        if is_int(input_str) and int(input_str) > 0:
-            return int(input_str)
-        else:
-            print('Ошибка ввода!')
-            
+        match number_type:
+            case 'int':
+                if is_int(input_str):
+                    return int(input_str)
+                else:
+                    print('Ошибка ввода!')
+            case 'natural':
+                if is_int(input_str) and int(input_str) > 0:
+                    return int(input_str)
+                else:
+                    print('Ошибка ввода!')
+            case 'float':
+                if is_int(input_str):
+                    return float(input_str)
+            case _:
+                raise AttributeError('Ошибка параметра функции!')
+
+def list_random_fill(size: int, min_border=0, max_border=10):
+    new_list = []
+    if size <= 0:
+        raise AttributeError('Ошибка параметра функции!')
+    if type(max_border)==int and type(min_border)==int:
+        for i in range(0,size):
+            new_list.append(random.randint(min_border, max_border))
+    elif type(max_border)==float or type(min_border)==float:
+        for i in range(0,size):
+            new_list.append(round(random.random()*\
+                random.randint(int(min_border), int(max_border)),3))
+    else:
+        raise AttributeError('Ошибка параметра функции!')
+    return new_list
+def dec_to_bin(dec_num:int):
+    bin_num = '000'
+    return bin_num
+def get_sum_odd_elements(num_list:list):
+    sum = 0
+    for num in num_list:
+        if num_list.index(num) % 2 !=0:
+            sum += num
+    return sum
+def is_float(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
 def is_int(str):
     try:
         int(str)
