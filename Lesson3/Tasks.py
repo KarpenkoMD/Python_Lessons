0a1893c94464ba49109790cb3837d46b14a6f4fc
mikhailka <karpenko.md@gmail.com>
2022-07-16 21:39:00 +0300

    case3 finished

--- Lesson3/Tasks.py
+++ Lesson3/Tasks.py
@@ -15,32 +15,36 @@ while True:
     match input_key:
         case '1':
             list_size = my_method.input_number('Введите количество элементов списка: ','natural')
-            list_numbers = my_method.list_random_fill(list_size,0,20)
+            list_numbers = my_method.list_random_fill(list_size,0,50)
             sum_odd_elements = my_method.get_sum_odd_elements(list_numbers)
-            result_string = 'Сумма нечетных элементов списка ' + str(list_numbers)\
+            output_result_string = 'Сумма нечетных элементов списка ' + str(list_numbers)\
                  + ' равна ' + str(sum_odd_elements)
             break
         case '2':
             list_size = my_method.input_number('Введите количество элементов списка: ','natural')
-            list_numbers = my_method.list_random_fill(list_size,0,20)
-            result_string = str(list_numbers) + str(list_size)
+            list_numbers = my_method.list_random_fill(list_size,0,10)
+            list_mult_pairs = my_method.mult_pair_elements(list_numbers)
+            output_result_string = 'Произведение пар чисел списка ' + str(list_numbers)\
+                 + ' равно ' + str(list_mult_pairs)
             break
         case '3':
             list_size = my_method.input_number('Введите количество элементов списка: ','natural')
-            list_numbers = my_method.list_random_fill(list_size,0,20.0)
-            result_string = str(list_numbers) + str(list_size)
+            list_numbers = my_method.list_random_fill(list_size,1,10.0)
+            difference = my_method.min_max_fraction_difference(list_numbers)
+            output_result_string = '\nРазница между максимальным и минимальным значением дробной части элементов списка\n'\
+                + str(list_numbers) + '\nравна ' + str(difference)
             break
         case '4':
             decimal_number = my_method.input_number(\
                 'Введите десятичное число для перевода в двоичное: ','int')
             binary_number = my_method.dec_to_bin(decimal_number)
-            result_string = 'Десятичное число ' + str(decimal_number) +\
+            output_result_string = 'Десятичное число ' + str(decimal_number) +\
                  ' = ' + binary_number + ' в двоичном виде'
             break
         case '5':
             number = my_method.input_number(\
                 'Введите число для ряда Фибоначчи: ','natural')
-            result_string = 'Задача 5'
+            output_result_string = 'Задача 5'
             break
         case 'q':
             input_key = None
@@ -51,4 +55,4 @@ while True:
 
 if input_key != None:
     print(f'Результат {input_key} задачи:\
-    {result_string}.\nДо свидания!')
+    {output_result_string}.\nДо свидания!')
--- Lesson3/my_method.py
+++ Lesson3/my_method.py
@@ -1,7 +1,8 @@
 import random
+from turtle import right
 
 
-def input_number(input_message, number_type = 'int'):
+def input_number(input_message, number_type='int'):
     while True:
         input_str = input(input_message)
         match number_type:
@@ -21,29 +22,7 @@ def input_number(input_message, number_type = 'int'):
             case _:
                 raise AttributeError('Ошибка параметра функции!')
 
-def list_random_fill(size: int, min_border=0, max_border=10):
-    new_list = []
-    if size <= 0:
-        raise AttributeError('Ошибка параметра функции!')
-    if type(max_border)==int and type(min_border)==int:
-        for i in range(0,size):
-            new_list.append(random.randint(min_border, max_border))
-    elif type(max_border)==float or type(min_border)==float:
-        for i in range(0,size):
-            new_list.append(round(random.random()*\
-                random.randint(int(min_border), int(max_border)),3))
-    else:
-        raise AttributeError('Ошибка параметра функции!')
-    return new_list
-def dec_to_bin(dec_num:int):
-    bin_num = '000'
-    return bin_num
-def get_sum_odd_elements(num_list:list):
-    sum = 0
-    for num in num_list:
-        if num_list.index(num) % 2 !=0:
-            sum += num
-    return sum
+
 def is_float(str):
     try:
         float(str)
@@ -51,9 +30,66 @@ def is_float(str):
     except ValueError:
         return False
 
+
 def is_int(str):
     try:
         int(str)
         return True
     except ValueError:
         return False
+
+
+def list_random_fill(size: int, min_border=0, max_border=10):
+    new_list = []
+    if size <= 0:
+        raise AttributeError('Ошибка параметра функции!')
+    if type(max_border) == int and type(min_border) == int:
+        for i in range(0, size):
+            new_list.append(random.randint(min_border, max_border))
+    elif type(max_border) == float or type(min_border) == float:
+        for i in range(0, size):
+            new_list.append(round(random.random() *
+                                  random.randint(int(min_border), int(max_border)), 3))
+    else:
+        raise AttributeError('Ошибка параметра функции!')
+    return new_list
+
+
+def get_sum_odd_elements(num_list: list):
+    sum = 0
+    for num in num_list:
+        if num_list.index(num) % 2 != 0:
+            sum += num
+    return sum
+
+
+def mult_pair_elements(num_list: list):
+    num_list_size = len(num_list)
+    mult_pair_list = []
+    for i in range(0, num_list_size//2):
+        mult_pair_list.append(num_list[i]*num_list[num_list_size-1-i])
+    if num_list_size % 2 != 0:
+        mult_pair_list.append(num_list[num_list_size//2]**2)
+    return mult_pair_list
+
+
+def min_max_fraction_difference(num_list: list):
+    if len(num_list) < 2:
+        raise AttributeError('Ошибка параметра функции!')
+    max = round(num_list[0] - int(num_list[0]),3)
+    min = round(num_list[1] - int(num_list[1]),3)
+    if max < min:
+        max, min = min, max
+    for i in range(2,len(num_list)):
+        temp = round(num_list[i]- int(num_list[i]),3)
+        if temp > max:
+             max = temp
+        elif temp< min:
+            min = temp
+    temp_str = str(round(max-min,3)).split('.')
+    return int(temp_str[1])
+
+
+def dec_to_bin(dec_num: int):
+    bin_num = '000'
+    return bin_num
