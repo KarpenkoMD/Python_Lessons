a05ed5e8d0eb371407b17f8d4a1703be905c3dff
mikhailka <karpenko.md@gmail.com>
2022-07-16 14:01:13 +0300

    Lesson2/Task4 time sleep added

--- Lesson2/Task4.py
+++ Lesson2/Task4.py
@@ -4,16 +4,31 @@
 Можете использовать xor, биты, библиотеку time (миллисекунды или наносекунды) - для задания случайности """
 
 import time 
+import random
 
 def my_random(min=0, max=10):
     result = min-1
     digit_num = len(str(max))
     while result < min or result > max:
         string_time = str(time.time_ns()//100)
-        result = int(string_time[len(string_time)-digit_num:len(string_time)])
+        ##print(string_time)
+        result = int(string_time[-digit_num: len(string_time)])
+        result = result >> 2
+        time.sleep(0.01)
+
     return result
 
 min_value = 1 
-max_value = 100
+max_value = 10
+##start = time.time_ns()
+for i in range(0,20):
+    start = time.time_ns()  
+    print(my_random(min_value,max_value), end =' ')
+    finish = time.time_ns()
+print('\n', finish-start)
 
-print(my_random(min_value,max_value))
+""" for i in range(0,20):
+    start = time.time_ns()  
+    print(random.randint(min_value,max_value), end =' ')
+    finish = time.time_ns()
+print('\n', finish-start)  """
